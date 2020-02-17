b0c81e59d2eaefac11a26c725b96b834fe2bf7b7
Kush Guglani <kush.guglani@nagarro.com>
2020-02-17 10:27:28 +0530

    mionor changes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
     // res.send('Hello World!')
     res.send(`<shipping_info>
     <item address1="Corso Buenos Aires" address2="" city="London" country="GB" first_name="Pluto" last_name="Qui" middle_name="" organization_name="" postal_code="WC2H 0JN" ship_mode_description="Standard delivery" state=""/>
-    <item address1="Corso Buenos Aires" address2="" city="London" country="GB" first_name="Pluto" last_name="Qui" middle_name="" organization_name="" postal_code="WC2H 0JN" ship_mode_description="Standard delivery" state=""/>
+    <item address1="Corso1" address2="" city="xyz" country="IN" first_name="John" last_name="martin" middle_name="" organization_name="" postal_code="WC2H 0JN2" ship_mode_description="Standard delivery" state=""/>
 </shipping_info>`)
 })
 
