0a97257bbe1df097d87ba1011a49d7c73bb8f285
Kush Guglani <kush.guglani@nagarro.com>
2020-02-27 17:22:07 +0530

    shiping info

--- index.js
+++ index.js
@@ -46,8 +46,7 @@ app.get('/', (req, res) => {
     res.type('application/xml');
     res.send(`<shipping_info>
     <item address1="Corso Buenos Aires" address2="" city="London" country="GB" first_name="Pluto" last_name="Qui" middle_name="" organization_name="" postal_code="WC2H 0JN" ship_mode_description="Standard delivery" state=""/>
-    <item address1="Corso1" address2="" city="xyz" country="IN" first_name="John" last_name="martin" middle_name="" organization_name="" postal_code="WC2H 0JN2" ship_mode_description="Standard delivery" state=""/>
-</shipping_info>`)
+   </shipping_info>`)
 })
 
 app.get('/payment_method', (req, res) => {
@@ -86,4 +85,6 @@ app.get('/order_items', (req, res) => {
 
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
